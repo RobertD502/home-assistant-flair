6283ff9b4dadfa5c9c1932821dccc01481464170
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2022-01-11 12:10:24 -0500

    rewrite vent integration

--- custom_components/flair/fan.py
+++ custom_components/flair/fan.py
@@ -1,6 +1,8 @@
 import logging
 from homeassistant.util.percentage import ordered_list_item_to_percentage
 from homeassistant.exceptions import PlatformNotReady
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 from homeassistant.components.fan import (
     FanEntity,
     SUPPORT_SET_SPEED,
@@ -11,15 +13,6 @@ from .const import (
     VENT_HALF_OPEN,
     VENT_OPEN
 )
-
-"""Attributes"""
-
-ATTR_PERCENT_OPEN = "percent_open"
-ATTR_IS_ACTIVE = "is_active"
-ATTR_RSSI = "rssi"
-ATTR_VOLTAGE = "voltage"
-ATTR_DUCT_TEMP = "duct_temp"
-ATTR_DUCT_PRESSURE = "duct_pressure_kPa"
 
 ORDERED_NAMED_VENT_STATES = [VENT_HALF_OPEN, VENT_OPEN]
 
@@ -33,11 +26,8 @@ HASS_FAN_SPEED_TO_FLAIR = {v: k for (k, v) in VENT_STATE_TO_HASS.items()}
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Platform uses config entry setup."""
-    pass
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities):
     """Set up Flair Vents."""
     flair = hass.data[DOMAIN]
 
@@ -56,7 +46,17 @@ class FlairVent(FanEntity):
 
     def __init__(self, vent):
         self._vent = vent
-        self._available = True
+
+    @property
+    def device_info(self):
+        """Return device registry information for this entity."""
+        return {
+            "identifiers": {(DOMAIN, self._vent.vent_id)},
+            "name": self._vent.vent_name,
+            "manufacturer": "Flair",
+            "model": "Flair Vent",
+            "configuration_url": "https://my.flair.co/",
+        }
 
     @property
     def unique_id(self):
@@ -77,60 +77,6 @@ class FlairVent(FanEntity):
     def is_on(self):
         """Return true if vent is open."""
         return self._vent.is_vent_open
-
-    @property
-    def percent_open(self) -> int:
-        if self._vent.vent_percent is None:
-            return None
-        return self._vent.vent_percent
-
-    @property
-    def is_active(self):
-        if self._vent.is_active is None:
-            return None
-        return self._vent.is_active
-
-    @property
-    def rssi(self) -> int:
-        if self._vent.rssi is None:
-            return None
-        return self._vent.rssi
-
-    @property
-    def voltage(self) -> int:
-        return self._vent.voltage
-
-    @property
-    def duct_temp(self):
-        if self._vent.duct_temp is None:
-            return None
-        elif not self.hass.config.units.is_metric:
-            return round(((self._vent.duct_temp * (9/5))+ 32), 2)
-        else:
-            return self._vent.duct_temp
-
-    @property
-    def duct_pressure(self):
-        if self._vent.duct_pressure is None:
-            return None
-        return self._vent.duct_pressure
-
-    @property
-    def available(self):
-        """Return true if device is available."""
-        return self._available
-
-    @property
-    def extra_state_attributes(self) -> dict:
-        """Return optional state attributes."""
-        return {
-            ATTR_PERCENT_OPEN: self.percent_open,
-            ATTR_DUCT_TEMP: self.duct_temp,
-            ATTR_DUCT_PRESSURE: self.duct_pressure,
-            ATTR_VOLTAGE: self.voltage,
-            ATTR_IS_ACTIVE: self.is_active,
-            ATTR_RSSI: self.rssi,
-        }
 
     @property
     def percentage(self) -> int:
@@ -166,6 +112,13 @@ class FlairVent(FanEntity):
         if not self.is_on:
             self._vent.set_vent_percentage(HASS_FAN_SPEED_TO_FLAIR.get(percentage))
         self._vent.set_vent_percentage(HASS_FAN_SPEED_TO_FLAIR.get(percentage))
+
+    @property
+    def available(self):
+        """Return true if device is available."""
+        if self._vent.is_active is None:
+            return False
+        return self._vent.is_active
 
     def update(self):
         """Update automation state."""
