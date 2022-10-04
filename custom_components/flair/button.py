a551f72f403acb174658d22e95dbed2872a346ad
Robert Drinovac <52541649+RobertD502@users.noreply.github.com>
2022-10-04 19:15:41 -0400

    add available property

--- custom_components/flair/button.py
+++ custom_components/flair/button.py
@@ -1,4 +1,4 @@
-""" Button platform for Flair integration."""
+"""Button platform for Flair integration."""
 from __future__ import annotations
 
 from typing import Any
@@ -18,21 +18,20 @@ from .coordinator import FlairDataUpdateCoordinator
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
-    """ Set Up Flair Button Entities. """
+    """Set Up Flair Button Entities."""
 
     coordinator: FlairDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
 
     buttons = []
 
-
     for structure_id, structure_data in coordinator.data.structures.items():
-            """ Structures """
+            # Structures
             buttons.extend((
                 HomeAwayClearHold(coordinator, structure_id),
                 HomeAwayRevert(coordinator, structure_id),
             ))
 
-            """ Rooms """
+            # Rooms
             if structure_data.rooms:
                 for room_id, room_data in structure_data.rooms.items():
                     buttons.extend((
@@ -43,22 +42,21 @@ async def async_setup_entry(
 
 
 class HomeAwayClearHold(CoordinatorEntity, ButtonEntity):
-    """ Representation of clearing home/away hold. """
+    """Representation of clearing home/away hold."""
 
     def __init__(self, coordinator, structure_id):
         super().__init__(coordinator)
         self.structure_id = structure_id
 
-
     @property
     def structure_data(self) -> Structure:
-        """ Handle coordinator structure data. """
+        """Handle coordinator structure data."""
 
         return self.coordinator.data.structures[self.structure_id]
 
     @property
     def device_info(self) -> dict[str, Any]:
-        """ Return device registry information for this entity. """
+        """Return device registry information for this entity."""
 
         return {
             "identifiers": {(DOMAIN, self.structure_data.id)},
@@ -70,24 +68,44 @@ class HomeAwayClearHold(CoordinatorEntity, ButtonEntity):
 
     @property
     def unique_id(self) -> str:
-        """ Sets unique ID for this entity. """
+        """Sets unique ID for this entity."""
 
         return str(self.structure_data.id) + '_home_away_clear_hold'
 
     @property
     def name(self) -> str:
-        """ Return name of the entity. """
+        """Return name of the entity."""
 
         return "Clear home/away hold"
 
     @property
     def has_entity_name(self) -> bool:
-        """ Indicate that entity has name defined. """
+        """Indicate that entity has name defined."""
 
         return True
 
+    @property
+    def entity_registry_enabled_default(self) -> bool:
+        """Disable entity if system mode is set to manual on initial registration."""
+
+        system_mode = self.structure_data.attributes['mode']
+        if system_mode == 'manual':
+            return False
+        else:
+            return True
+
+    @property
+    def available(self) -> bool:
+        """Marks entity as unavailable if system mode is set to Manual."""
+
+        system_mode = self.structure_data.attributes['mode']
+        if system_mode == 'manual':
+            return False
+        else:
+            return True
+
     async def async_press(self) -> None:
-        """ Handle the button press. """
+        """Handle the button press."""
 
         attributes = {
             "hold-until": None
@@ -98,23 +116,23 @@ class HomeAwayClearHold(CoordinatorEntity, ButtonEntity):
         self.async_write_ha_state()
         await self.coordinator.async_request_refresh()
 
+
 class HomeAwayRevert(CoordinatorEntity, ButtonEntity):
-    """ Representation of clearing home/away hold and reverting to previous state. """
+    """Representation of clearing home/away hold and reverting to previous state."""
 
     def __init__(self, coordinator, structure_id):
         super().__init__(coordinator)
         self.structure_id = structure_id
 
-
     @property
     def structure_data(self) -> Structure:
-        """ Handle coordinator structure data. """
+        """Handle coordinator structure data."""
 
         return self.coordinator.data.structures[self.structure_id]
 
     @property
     def device_info(self) -> dict[str, Any]:
-        """ Return device registry information for this entity. """
+        """Return device registry information for this entity."""
 
         return {
             "identifiers": {(DOMAIN, self.structure_data.id)},
@@ -126,24 +144,44 @@ class HomeAwayRevert(CoordinatorEntity, ButtonEntity):
 
     @property
     def unique_id(self) -> str:
-        """ Sets unique ID for this entity. """
+        """Sets unique ID for this entity."""
 
         return str(self.structure_data.id) + '_home_away_reverse'
 
     @property
     def name(self) -> str:
-        """ Return name of the entity. """
+        """Return name of the entity."""
 
         return "Reverse home/away hold"
 
     @property
     def has_entity_name(self) -> bool:
-        """ Indicate that entity has name defined. """
+        """Indicate that entity has name defined."""
 
         return True
 
+    @property
+    def entity_registry_enabled_default(self) -> bool:
+        """Disable entity if system mode is set to manual on initial registration."""
+
+        system_mode = self.structure_data.attributes['mode']
+        if system_mode == 'manual':
+            return False
+        else:
+            return True
+
+    @property
+    def available(self) -> bool:
+        """Marks entity as unavailable if system mode is set to Manual."""
+
+        system_mode = self.structure_data.attributes['mode']
+        if system_mode == 'manual':
+            return False
+        else:
+            return True
+
     async def async_press(self) -> None:
-        """ Handle the button press. """
+        """Handle the button press."""
 
         home_attributes, hold_attributes = self.set_attributes()
         await self.coordinator.client.update('structures', self.structure_data.id, home_attributes, relationships={})
@@ -154,7 +192,7 @@ class HomeAwayRevert(CoordinatorEntity, ButtonEntity):
         await self.coordinator.async_request_refresh()
 
     def set_attributes(self) -> dict[str, Any]:
-        """ Creates attributes dictionary. """
+        """Creates attributes dictionary."""
 
         is_home = self.structure_data.attributes['home']
         if is_home:
@@ -172,24 +210,30 @@ class HomeAwayRevert(CoordinatorEntity, ButtonEntity):
 
         return home_attributes, hold_attributes
 
+
 class RoomClearHold(CoordinatorEntity, ButtonEntity):
-    """ Representation of clearing room temperature hold. """
+    """Representation of clearing room temperature hold."""
 
     def __init__(self, coordinator, structure_id, room_id):
         super().__init__(coordinator)
         self.room_id = room_id
         self.structure_id = structure_id
 
-
     @property
     def room_data(self) -> Room:
-        """ Handle coordinator room data. """
+        """Handle coordinator room data."""
 
         return self.coordinator.data.structures[self.structure_id].rooms[self.room_id]
 
     @property
+    def structure_data(self) -> Structure:
+        """Handle coordinator structure data."""
+
+        return self.coordinator.data.structures[self.structure_id]
+
+    @property
     def device_info(self) -> dict[str, Any]:
-        """ Return device registry information for this entity. """
+        """Return device registry information for this entity."""
 
         return {
             "identifiers": {(DOMAIN, self.room_data.id)},
@@ -201,24 +245,44 @@ class RoomClearHold(CoordinatorEntity, ButtonEntity):
 
     @property
     def unique_id(self) -> str:
-        """ Sets unique ID for this entity. """
+        """Sets unique ID for this entity."""
 
         return str(self.room_data.id) + '_clear_hold'
 
     @property
     def name(self) -> str:
-        """ Return name of the entity. """
+        """Return name of the entity."""
 
         return "Clear hold"
 
     @property
     def has_entity_name(self) -> bool:
-        """ Indicate that entity has name defined. """
+        """Indicate that entity has name defined."""
 
         return True
 
+    @property
+    def entity_registry_enabled_default(self) -> bool:
+        """Disable entity if system mode is set to manual on initial registration."""
+
+        system_mode = self.structure_data.attributes['mode']
+        if system_mode == 'manual':
+            return False
+        else:
+            return True
+
+    @property
+    def available(self) -> bool:
+        """Marks entity as unavailable if system mode is set to Manual."""
+
+        system_mode = self.structure_data.attributes['mode']
+        if system_mode == 'manual':
+            return False
+        else:
+            return True
+
     async def async_press(self) -> None:
-        """ Handle the button press. """
+        """Handle the button press."""
 
         attributes = {
             "hold-until": None,
