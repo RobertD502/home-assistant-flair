7c51e053e1f84d3ef6ecc5c5d60a81d48a009e4b
Robert Drinovac <52541649+RobertD502@users.noreply.github.com>
2022-10-04 19:24:11 -0400

    only add IR Lock entity if HVAC unit present

--- custom_components/flair/switch.py
+++ custom_components/flair/switch.py
@@ -1,12 +1,11 @@
-""" Switch platform for Flair integration. """
+"""Switch platform for Flair integration."""
 from __future__ import annotations
 
 from typing import Any
 
-
 from flairaio.model import Puck, Structure
-from homeassistant.components.switch import SwitchEntity
 
+from homeassistant.components.switch import SwitchEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -15,30 +14,31 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .const import DOMAIN
 from .coordinator import FlairDataUpdateCoordinator
 
+
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
-    """ Set Up Flair Switch Entities. """
+    """Set Up Flair Switch Entities."""
 
     coordinator: FlairDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
 
     switches = []
 
-
     for structure_id, structure_data in coordinator.data.structures.items():
-            """ Structures """
-            switches.extend((
-                LockIR(coordinator, structure_id),
-            ))
+            # Structures
+            if structure_data.hvac_units:
+                switches.extend((
+                    LockIR(coordinator, structure_id),
+                ))
             
-            """ Pucks """
+            # Pucks
             if structure_data.pucks:
                 for puck_id, puck_data in structure_data.pucks.items():
                     switches.extend((
                         PuckLock(coordinator, structure_id, puck_id),
                     ))
 
-            """ HVAC Units """
+            # HVAC Units
             if structure_data.hvac_units:
                 for hvac_id, hvac_data in structure_data.hvac_units.items():
                     try:
@@ -56,22 +56,21 @@ async def async_setup_entry(
 
 
 class LockIR(CoordinatorEntity, SwitchEntity):
-    """ Representation of Structure HVAC IR lock. """
+    """Representation of Structure HVAC IR lock."""
 
     def __init__(self, coordinator, structure_id):
         super().__init__(coordinator)
         self.structure_id = structure_id
 
-
     @property
     def structure_data(self) -> Structure:
-        """ Handle coordinator structure data. """
+        """Handle coordinator structure data."""
 
         return self.coordinator.data.structures[self.structure_id]
 
     @property
     def device_info(self) -> dict[str, Any]:
-        """ Return device registry information for this entity. """
+        """Return device registry information for this entity."""
 
         return {
             "identifiers": {(DOMAIN, self.structure_data.id)},
@@ -83,27 +82,28 @@ class LockIR(CoordinatorEntity, SwitchEntity):
 
     @property
     def unique_id(self) -> str:
-        """ Sets unique ID for this entity. """
+        """Sets unique ID for this entity."""
 
         return str(self.structure_data.id) + '_IR_lock'
 
     @property
     def name(self) -> str:
-        """ Return name of the entity. """
+        """Return name of the entity."""
 
         return "Lock IR device modes"
 
     @property
     def has_entity_name(self) -> bool:
-        """ Indicate that entity has name defined. """
+        """Indicate that entity has name defined."""
 
         return True
 
     @property
     def icon(self) -> str:
-        """ Set icon. """
+        """Set icon."""
 
         ir_locked = self.structure_data.attributes['hvac-unit-group-lock']
+
         if ir_locked:
             return 'mdi:lock'
         else:
@@ -111,21 +111,12 @@ class LockIR(CoordinatorEntity, SwitchEntity):
 
     @property
     def is_on(self) -> bool:
-        """ Return true if IR is locked. """
+        """Determine if IR is locked."""
 
         return self.structure_data.attributes['hvac-unit-group-lock']
 
-    @property
-    def available(self) -> str:
-        """ Return true if account has HVAC units. """
-
-        if self.structure_data.hvac_units:
-            return True
-        else:
-            return False
-
     async def async_turn_on(self, **kwargs):
-        """ Lock the IR devices. """
+        """Lock the IR devices."""
 
         attributes = {"hvac-unit-group-lock": True}
         await self.coordinator.client.update('structures', self.structure_data.id, attributes=attributes, relationships={})
@@ -134,7 +125,7 @@ class LockIR(CoordinatorEntity, SwitchEntity):
         await self.coordinator.async_request_refresh()
 
     async def async_turn_off(self, **kwargs):
-        """ Unlock the IR devices. """
+        """Unlock the IR devices."""
 
         attributes = {"hvac-unit-group-lock": False}
         await self.coordinator.client.update('structures', self.structure_data.id, attributes=attributes, relationships={})
@@ -144,23 +135,22 @@ class LockIR(CoordinatorEntity, SwitchEntity):
 
 
 class PuckLock(CoordinatorEntity, SwitchEntity):
-    """ Representation of puck lock switch. """
+    """Representation of puck lock switch."""
 
     def __init__(self, coordinator, structure_id, puck_id):
         super().__init__(coordinator)
         self.puck_id = puck_id
         self.structure_id = structure_id
 
-
     @property
     def puck_data(self) -> Puck:
-        """ Handle coordinator puck data. """
+        """Handle coordinator puck data."""
 
         return self.coordinator.data.structures[self.structure_id].pucks[self.puck_id]
 
     @property
     def device_info(self) -> dict[str, Any]:
-        """ Return device registry information for this entity. """
+        """Return device registry information for this entity."""
 
         return {
             "identifiers": {(DOMAIN, self.puck_data.id)},
@@ -172,27 +162,28 @@ class PuckLock(CoordinatorEntity, SwitchEntity):
 
     @property
     def unique_id(self) -> str:
-        """ Sets unique ID for this entity. """
+        """Sets unique ID for this entity."""
 
         return str(self.puck_data.id) + '_puck_lock'
 
     @property
     def name(self) -> str:
-        """ Return name of the entity. """
+        """Return name of the entity."""
 
         return "Lock puck"
 
     @property
     def has_entity_name(self) -> bool:
-        """ Indicate that entity has name defined. """
+        """Indicate that entity has name defined."""
 
         return True
 
     @property
     def icon(self) -> str:
-        """ Set icon. """
+        """Set icon."""
 
         puck_locked = self.puck_data.attributes['locked']
+
         if puck_locked:
             return 'mdi:lock'
         else:
@@ -200,25 +191,28 @@ class PuckLock(CoordinatorEntity, SwitchEntity):
 
     @property
     def is_on(self) -> bool:
-        """ Return if puck is locked. """
+        """Return if puck is locked."""
 
         return self.puck_data.attributes['locked']
 
     @property
     def available(self) -> str:
-        """ Return true if puck is active and
-        puck lock setting isn't None within the Flair app.
+        """Determine if puck is available.
+
+        Return true if puck is active and puck lock setting
+        isn't None within the Flair app.
         """
 
         puck_inactive = self.puck_data.attributes['inactive']
         puck_locked = self.puck_data.attributes['locked']
+
         if (puck_inactive == False) and (puck_locked is not None):
             return True
         else:
             return False
 
     async def async_turn_on(self, **kwargs):
-        """ Lock the puck. """
+        """Lock the puck."""
 
         attributes = {"locked": True}
         await self.coordinator.client.update('pucks', self.puck_data.id, attributes=attributes, relationships={})
@@ -227,7 +221,7 @@ class PuckLock(CoordinatorEntity, SwitchEntity):
         await self.coordinator.async_request_refresh()
 
     async def async_turn_off(self, **kwargs):
-        """ Unlock the puck. """
+        """Unlock the puck."""
 
         attributes = {"locked": False}
         await self.coordinator.client.update('pucks', self.puck_data.id, attributes=attributes, relationships={})
@@ -235,44 +229,43 @@ class PuckLock(CoordinatorEntity, SwitchEntity):
         self.async_write_ha_state()
         await self.coordinator.async_request_refresh()
 
+
 class HVACPower(CoordinatorEntity, SwitchEntity):
-    """ Representation of HVAC Unit Power. """
+    """Representation of HVAC Unit Power."""
 
     def __init__(self, coordinator, structure_id, hvac_id):
         super().__init__(coordinator)
         self.hvac_id = hvac_id
         self.structure_id = structure_id
 
-
     @property
     def hvac_data(self) -> HVACUnit:
-        """ Handle coordinator HVAC unit data. """
+        """Handle coordinator HVAC unit data."""
 
         return self.coordinator.data.structures[self.structure_id].hvac_units[self.hvac_id]
 
     @property
     def structure_data(self) -> Structure:
-        """ Handle coordinator structure data. """
+        """Handle coordinator structure data."""
 
         return self.coordinator.data.structures[self.structure_id]
 
     @property
     def puck_data(self) -> Puck:
-        """ Handle coordinator puck data. """
+        """Handle coordinator puck data."""
 
         puck_id = self.hvac_data.relationships['puck']['data']['id']
-
         return self.coordinator.data.structures[self.structure_id].pucks[puck_id]
 
     @property
     def structure_mode(self) -> str:
-        """ Return structure mode of associated structure. """
+        """Return structure mode of associated structure."""
 
         return self.structure_data.attributes['mode']
 
     @property
     def device_info(self) -> dict[str, Any]:
-        """ Return device registry information for this entity. """
+        """Return device registry information for this entity."""
 
         return {
             "identifiers": {(DOMAIN, self.hvac_data.id)},
@@ -284,48 +277,51 @@ class HVACPower(CoordinatorEntity, SwitchEntity):
 
     @property
     def unique_id(self) -> str:
-        """ Sets unique ID for this entity. """
+        """Sets unique ID for this entity."""
 
         return str(self.hvac_data.id) + '_hvac_power'
 
     @property
     def name(self) -> str:
-        """ Return name of the entity. """
+        """Return name of the entity."""
 
         return "HVAC power"
 
     @property
     def has_entity_name(self) -> bool:
-        """ Indicate that entity has name defined. """
+        """Indicate that entity has name defined."""
 
         return True
 
     @property
     def icon(self) -> str:
-        """ Set hvac icon. """
+        """Set hvac icon."""
 
         return 'mdi:hvac'
 
     @property
     def is_on(self) -> bool:
-        """ Return if HVAC unit is on. """
+        """Return if HVAC unit is on."""
 
         return self.hvac_data.attributes['power'] == 'On'
 
     @property
     def available(self) -> str:
-        """ Return true if puck is active and
+        """Determine if puck is active.
+
+        Return true if puck is active and
         structure is set to manual mode.
         """
 
         puck_inactive = self.puck_data.attributes['inactive']
+
         if (puck_inactive == False) and (self.structure_mode == 'manual'):
             return True
         else:
             return False
 
     async def async_turn_on(self, **kwargs):
-        """ Turn on HVAC unit. """
+        """Turn on HVAC unit."""
 
         attributes = {"power": "On"}
         await self.coordinator.client.update('hvac-units', self.hvac_data.id, attributes=attributes, relationships={})
@@ -334,7 +330,7 @@ class HVACPower(CoordinatorEntity, SwitchEntity):
         await self.coordinator.async_request_refresh()
 
     async def async_turn_off(self, **kwargs):
-        """ Turn off HVAC unit. """
+        """Turn off HVAC unit."""
 
         attributes = {"power": "Off"}
         await self.coordinator.client.update('hvac-units', self.hvac_data.id, attributes=attributes, relationships={})
